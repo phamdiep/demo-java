5de818c4cee4e44ca44601857a98ed9dca99d2af
phamdiep <dieppv0101@gmail.com>
2024-10-15 15:47:27 +0700

    Update AddressBook.java
    go for it.

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// diep testing
+// diep testing, testing 3
 public class AddressBook {
     static ArrayList<Contact> contacts = new ArrayList<Contact>();
     static Scanner input = new Scanner(System.in);
